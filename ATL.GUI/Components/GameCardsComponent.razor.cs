22823e453725598d093e922178b9c92cc0f88b45
EonZeNx <kf4open@pm.me>
2024-10-30 13:30:49 +1300

    feat(): added game cards to home page

---
+++ ATL.GUI/Components/GameCardsComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ATL.GUI.Components;
+
+public partial class GameCardsComponent : ComponentBase
+{
+    [Parameter]
+    public string SelectedGameId { get; set; } = "jc3";
+    
+    [Parameter]
+    public Action<string> SelectedGameChanged { get; set; }
+
+    protected void OnGameCardClicked(string gameId)
+    {
+        SelectedGameId = gameId;
+        SelectedGameChanged(gameId);
+        StateHasChanged();
+    }
+
+    protected List<string> TestGameIds = ["jc3", "jc4", "jc5", "jc6"];
+}
