a0df66bc07a20c73588ef7b0126019af1d697571
EonZeNx <kf4open@pm.me>
2024-11-06 23:01:00 +1300

    feat: convert game to euler

---
+++ ApexToolsLauncher.Core/Extensions/MathExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ApexToolsLauncher.Core.Extensions;
+
+public static class MathExtensions
+{
+    public static bool AlmostEqual(this float a, float b, float delta = 0.00001f)
+    {
+        return Math.Abs(a - b) < delta;
+    }
+}
