8705c60efb0acb8e2a1186963105908389a4ca73
Kieran <kf4open@pm.me>
2024-12-31 20:18:07 +1000

    fix: import for parallel

--- ApexToolsLauncher.CLI/Program.cs
+++ ApexToolsLauncher.CLI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using ApexChain.AAFSARC;
 using ApexFormat.AAF.V01;
 using ApexFormat.ADF.V04;
