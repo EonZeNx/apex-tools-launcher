9c8f2cea8b4b43373a6917b8406abae995b8cce0
Kieran <kf4open@pm.me>
2024-12-31 17:36:07 +1000

    feat: updated ICv01 xml, looks closer to RTPC

--- Formats/ApexFormat.IC.V01/Class/IcV01Collection.cs
+++ Formats/ApexFormat.IC.V01/Class/IcV01Collection.cs
@@ -1,7 +1,7 @@
 ﻿using System.Xml.Linq;
 using ApexFormat.IC.V01.Enum;
 using ApexToolsLauncher.Core.Class;
-using ApexToolsLauncher.Core.Hash;
+using ApexToolsLauncher.Core.Libraries;
 using CommunityToolkit.HighPerformance;
 using RustyOptions;
 
@@ -9,18 +9,21 @@ namespace ApexFormat.IC.V01.Class;
 
 /// <summary>
 /// Structure:
-/// <br/>Type - <see cref="EIcV01ContainerType"/>
+/// <br/>Type - <see cref="EIcV01CollectionType"/>
 /// <br/>Count - <see cref="ushort"/>
+/// <br/>Collections OR Properties - <see cref="ushort"/>
 /// </summary>
 public class IcV01Collection : ISizeOf
 {
-    public uint NameHash = 0;
-    public byte Count = 0;
+    public EIcV01CollectionType Type = EIcV01CollectionType.Unk0;
+    public ushort Count = 0;
+
     public IcV01Container[] Containers = [];
+    public IcV01Property[] Properties = [];
 
     public static uint SizeOf()
     {
-        return sizeof(EIcV01ContainerType) + // Type
+        return sizeof(EIcV01CollectionType) + // Type
                sizeof(ushort); // Count
     }
 }
@@ -33,43 +36,84 @@ public static class IcV01CollectionExtensions
         {
             return Option<IcV01Collection>.None;
         }
-
+        
         var result = new IcV01Collection
         {
-            NameHash = stream.Read<uint>(),
-            Count = stream.Read<byte>(),
+            Type = stream.Read<EIcV01CollectionType>(),
+            Count = stream.Read<ushort>()
         };
-        
-        result.Containers = new IcV01Container[result.Count];
-        for (var i = 0; i < result.Count; i++)
-        {
-            var optionContainer = stream.ReadIcV01Container();
-            if (optionContainer.IsSome(out var container))
-                result.Containers[i] = container;
-        }
 
+        switch (result.Type)
+        {
+            case EIcV01CollectionType.Unk0:
+            case EIcV01CollectionType.Container:
+            {
+                result.Containers = new IcV01Container[result.Count];
+                for (var i = 0; i < result.Count; i++)
+                {
+                    var optionCollection = stream.ReadIcV01Container();
+                    if (optionCollection.IsSome(out var collection))
+                        result.Containers[i] = collection;
+                }
+                break;
+            }
+            case EIcV01CollectionType.Property:
+            {
+                result.Properties = new IcV01Property[result.Count];
+                for (var i = 0; i < result.Count; i++)
+                {
+                    var optionProperty = stream.ReadIcV01Property();
+                    if (optionProperty.IsSome(out var property))
+                        result.Properties[i] = property;
+                }
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
         return Option.Some(result);
     }
     
     public static XElement ToXElement(this IcV01Collection collection)
     {
         var xe = new XElement("collection");
-        
-        var optionHashResult = HashDatabases.Lookup(collection.NameHash);
-        if (optionHashResult.IsSome(out var hashResult))
-        {
-            xe.SetAttributeValue("name", hashResult.Value);
-        }
-        else
-        {
-            xe.SetAttributeValue("id", $"{collection.NameHash:X8}");
-        }
+        xe.SetAttributeValue("type", collection.Type.ToXmlString());
 
-        foreach (var container in collection.Containers)
+        if (collection.Containers.Length != 0)
         {
-            xe.Add(container.ToXElement());
+            foreach (var container in collection.Containers)
+            {
+                xe.Add(container.ToXElement());
+            }
+        }
+        else if (collection.Properties.Length != 0)
+        {
+            foreach (var property in collection.Properties)
+            {
+                xe.Add(property.ToXElement());
+            }
         }
 
         return xe;
+    }
+    
+    public static XElement[] ToXElements(this IcV01Collection collection)
+    {
+        var xeList = new List<XElement>();
+
+        if (collection.Containers.Length != 0)
+        {
+            xeList.AddRange(collection.Containers.Select(c => c.ToXElement()));
+        }
+        else if (collection.Properties.Length != 0)
+        {
+            xeList.AddRange(collection.Properties.Select(p => p.ToXElement()));
+        }
+        
+        var xes = xeList.ToArray();
+        Array.Sort(xes, XDocumentLibrary.SortNameThenId);
+
+        return xes;
     }
 }
---
+++ Formats/ApexFormat.IC.V01/Enum/EIcV01CollectionType.cs
@@ -0,0 +1,23 @@
+﻿namespace ApexFormat.IC.V01.Enum;
+
+public enum EIcV01CollectionType : ushort
+{
+    Unk0       = 0x03,
+    Container  = 0x04,
+    Property   = 0x05,
+}
+
+public static class EIcV01CollectionTypeExtensions
+{
+    public static Dictionary<EIcV01CollectionType, string> TypeToXmlString = new()
+    {
+        { EIcV01CollectionType.Unk0,       "unk0" },
+        { EIcV01CollectionType.Container,  "container" },
+        { EIcV01CollectionType.Property,   "property" },
+    };
+
+    public static string ToXmlString(this EIcV01CollectionType collectionType)
+    {
+        return TypeToXmlString.GetValueOrDefault(collectionType, "failed");
+    }
+}
