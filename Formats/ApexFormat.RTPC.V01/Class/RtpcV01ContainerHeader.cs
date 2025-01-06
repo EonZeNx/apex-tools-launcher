6dc088fc1cf224b2e93fbb753f7e0fb2d99cb1f3
Kieran <kf4open@pm.me>
2025-01-07 09:55:34 +1000

    feat: switch RTPC v01 to extract repack interfaces

--- Formats/ApexFormat.RTPC.V01/Class/RtpcV01ContainerHeader.cs
+++ Formats/ApexFormat.RTPC.V01/Class/RtpcV01ContainerHeader.cs
@@ -1,5 +1,5 @@
 ﻿using System.Xml.Linq;
-using ApexToolsLauncher.Core.Class;
+using ApexToolsLauncher.Core.Extensions;
 using CommunityToolkit.HighPerformance;
 using RustyOptions;
 
@@ -13,27 +13,24 @@ namespace ApexFormat.RTPC.V01.Class;
 /// <br/>PropertyCount - <see cref="ushort"/>
 /// <br/>ContainerCount - <see cref="ushort"/>
 /// </summary>
-public class RtpcV01ContainerHeader : ISizeOf
+public class RtpcV01ContainerHeader
 {
     public uint NameHash = 0;
     public uint Offset = 0;
     public ushort PropertyCount = 0;
     public ushort ContainerCount = 0;
-
-    public static uint SizeOf()
-    {
-        return sizeof(uint) + // NameHash
-               sizeof(uint) + // Offset
-               sizeof(ushort) + // PropertyCount
-               sizeof(ushort); // ContainerCount
-    }
 }
 
-public static class RtpcV01ContainerHeaderExtensions
+public static class RtpcV01ContainerHeaderLibrary
 {
+    public const int SizeOf = sizeof(uint) // NameHash
+                              + sizeof(uint) // Offset
+                              + sizeof(ushort) // PropertyCount
+                              + sizeof(ushort); // ContainerCount
+    
     public static Option<RtpcV01ContainerHeader> ReadRtpcV01ContainerHeader(this Stream stream)
     {
-        if (stream.Length - stream.Position < RtpcV01ContainerHeader.SizeOf())
+        if (stream.CouldRead(SizeOf))
         {
             return Option<RtpcV01ContainerHeader>.None;
         }
@@ -49,7 +46,7 @@ public static class RtpcV01ContainerHeaderExtensions
         return Option.Some(result);
     }
     
-    public static XElement WriteXElement(this RtpcV01ContainerHeader container)
+    public static XElement ToXElement(this RtpcV01ContainerHeader container)
     {
         var xe = new XElement("object");
         xe.SetAttributeValue("id", $"{container.NameHash:X8}");
--- Formats/ApexFormat.RTPC.V01/Class/RtpcV01VariantHeader.cs
+++ Formats/ApexFormat.RTPC.V01/Class/RtpcV01VariantHeader.cs
@@ -1,5 +1,4 @@
 ﻿using ApexFormat.RTPC.V01.Enum;
-using ApexToolsLauncher.Core.Class;
 using ApexToolsLauncher.Core.Extensions;
 using CommunityToolkit.HighPerformance;
 using RustyOptions;
@@ -12,19 +11,12 @@ namespace ApexFormat.RTPC.V01.Class;
 /// <br/>Data - <see cref="byte"/>[4]
 /// <br/>VariantType - <see cref="ERtpcV01VariantType"/>
 /// </summary>
-public class RtpcV01VariantHeader : ISizeOf
+public class RtpcV01VariantHeader
 {
     public uint NameHash = 0;
     public byte[] Data = [4];
     public ERtpcV01VariantType VariantType = ERtpcV01VariantType.Unassigned;
     
-    public static uint SizeOf()
-    {
-        return sizeof(uint) + // NameHash
-               4 + // Data
-               sizeof(ERtpcV01VariantType);  // VariantType
-    }
-
     public override string ToString()
     {
         return $"{NameHash:X8} ({VariantType})";
@@ -33,9 +25,13 @@ public class RtpcV01VariantHeader : ISizeOf
 
 public static class RtpcV01VariantHeaderExtensions
 {
+    public const int SizeOf = sizeof(uint) // NameHash
+                              + 4 // Data
+                              + sizeof(ERtpcV01VariantType);  // VariantType
+    
     public static Option<RtpcV01VariantHeader> ReadRtpcV01VariantHeader(this Stream stream)
     {
-        if (stream.Length - stream.Position < RtpcV01VariantHeader.SizeOf())
+        if (stream.CouldRead(SizeOf))
         {
             return Option<RtpcV01VariantHeader>.None;
         }
