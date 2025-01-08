9d36cf577f8235db875fe719b90472c30db8a93e
EonZeNx <kf4open@pm.me>
2025-01-08 10:36:21 +1000

    feat: checkpoint towards profile editing

--- ApexToolsLauncher.GUI/Components/Panels/ProfilesTabPanel.razor.cs
+++ ApexToolsLauncher.GUI/Components/Panels/ProfilesTabPanel.razor.cs
@@ -1,13 +1,17 @@
 using ApexToolsLauncher.Core.Config.GUI;
 using ApexToolsLauncher.Core.Libraries;
+using ApexToolsLauncher.GUI.Services.App;
 using ApexToolsLauncher.GUI.Services.Mod;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace ApexToolsLauncher.GUI.Components.Panels;
 
-public partial class ProfilesTabPanel : MudComponentBase
+public partial class ProfilesTabPanel : MudComponentBase, IDisposable
 {
+    [Inject]
+    protected IAppStateService? AppStateService { get; set; }
+    
     [Inject]
     protected IProfileConfigService? ProfileConfigService { get; set; }
     
@@ -17,57 +21,55 @@ public partial class ProfilesTabPanel : MudComponentBase
     [Parameter]
     public string ProfileId { get; set; } = ConstantsLibrary.InvalidString;
     
-    [Parameter]
-    public string LaunchId { get; set; } = ConstantsLibrary.InvalidString;
-    
-    [Parameter]
-    public LaunchOptionConfig LaunchOptionConfig { get; set; } = new();
-    
-    public ProfileConfig ProfileConfig { get; set; } = new();
-    
-    public void OnLaunchOptionToggle(bool isOn)
-    {
-        if (ProfileConfigService is null) return;
-        
-        if (isOn)
-        {
-            ProfileConfig.LaunchArguments.Add(LaunchId, new Dictionary<string, string>());
-        }
-        else
-        {
-            ProfileConfig.LaunchArguments.Remove(LaunchId);
-        }
-        
-        ProfileConfigService.Save(GameId, ProfileId, ProfileConfig);
-    }
-    
-    public void LaunchArgumentChanged(string key, string value)
-    {
-        if (ProfileConfigService is null) return;
-        
-        if (!ProfileConfig.LaunchArguments.TryGetValue(LaunchId, out var launchConfig))
-        {
-            return;
-        }
+    protected ProfileConfig ProfileConfig { get; set; } = new();
+    protected Dictionary<string, ProfileConfig> ProfileConfigs { get; set; } = [];
 
-        if (!launchConfig.TryAdd(key, value))
-        {
-            launchConfig[key] = value;
-        }
+    protected void ListValueChanged(string? value)
+    {
+        if (value is null) return;
 
-        ProfileConfig.LaunchArguments[LaunchId] = launchConfig;
-        ProfileConfigService.Save(GameId, ProfileId, ProfileConfig);
+        ProfileId = value;
+        ReloadData();
+        StateHasChanged();
     }
-    
+
     protected void ReloadData()
     {
+        if (AppStateService is null) return;
         if (ProfileConfigService is null) return;
         
+        ProfileConfigs = ProfileConfigService.GetAllFromGame(GameId);
+        if (!ProfileConfigs.ContainsKey(ProfileId) && !ConstantsLibrary.IsStringInvalid(ProfileId))
+        {
+            ProfileId = ConstantsLibrary.InvalidString;
+        }
+        
         ProfileConfig = ProfileConfigService.Get(GameId, ProfileId);
+
+        var profileId = AppStateService.GetLastProfileId(GameId);
+        ProfileConfig = ProfileConfigService.Get(GameId, profileId);
     }
     
     protected override async Task OnParametersSetAsync()
     {
         await Task.Run(ReloadData);
+    }
+    
+    protected async void OnConfigReloaded()
+    {
+        await Task.Run(ReloadData);
+        await InvokeAsync(StateHasChanged);
+    }
+    
+    protected override void OnInitialized()
+    {
+        AppStateService?.RegisterOnReload(OnConfigReloaded);
+        ProfileConfigService?.RegisterOnReload(OnConfigReloaded);
+    }
+
+    public void Dispose()
+    {
+        AppStateService?.UnregisterOnReload(OnConfigReloaded);
+        ProfileConfigService?.UnregisterOnReload(OnConfigReloaded);
     }
 }
