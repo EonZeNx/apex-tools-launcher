b1799cfc47e6127ecdb92e4fee7e1d16c3928a5b
EonZeNx <kf4open@pm.me>
2025-01-08 10:35:32 +1000

    AVTXv01 progress

--- ApexFormats/ApexFormat.AVTX.V01/Enum/EAvtxV01TextureFlags.cs
+++ ApexFormats/ApexFormat.AVTX.V01/Enum/EAvtxV01TextureFlags.cs
@@ -3,10 +3,10 @@
 [Flags]
 public enum EAvtxV01TextureFlags : uint
 {
-    Streamed      = 0x1,
-    Placement     = 0x2,
-    Tiled         = 0x4,
-    sRGB          = 0x8,
+    Streamed      = 0x01,
+    Placement     = 0x02,
+    Tiled         = 0x04,
+    sRGB          = 0x08,
     LodFromRender = 0x10,
     Cube          = 0x40,
     Watch         = 0x8000,
