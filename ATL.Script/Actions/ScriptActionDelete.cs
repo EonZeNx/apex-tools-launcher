2f7322b62cbc33f318e6c4a3e5a85454e742b0bb
EonZeNx <kf4open@pm.me>
2025-01-08 10:35:33 +1000

    Added ScriptActionMove and ScriptActionDelete

---
+++ ATL.Script/Actions/ScriptActionDelete.cs
@@ -0,0 +1,64 @@
+﻿using System.Xml.Linq;
+using ATL.Core.Extensions;
+using ATL.Core.Libraries;
+using ATL.Script.Variables;
+using RustyOptions;
+
+namespace ATL.Script.Actions;
+
+public class ScriptActionDelete : IScriptAction
+{
+    public static string NodeName { get; } = "delete";
+    
+    public void Process(XElement element, Dictionary<string, ScriptVariable> variables)
+    {
+        var targetAttr = element.Attribute("target");
+        if (targetAttr is null)
+            return;
+        
+        var targetPath = targetAttr.Value;
+        if (targetPath.StartsWith(ScriptVariable.NodeSymbol))
+        {
+            var targetVarName = targetPath.Replace(ScriptVariable.NodeSymbol, "");
+            
+            var optionTargetVar = variables.GetValueOrNone(targetVarName);
+            if (!optionTargetVar.IsSome(out var targetFromVar))
+            {
+                return;
+            }
+
+            var optionTarget = targetFromVar.AsString();
+            if (!optionTarget.IsSome(out var targetVar))
+            {
+                return;
+            }
+            
+            targetPath = targetVar;
+        }
+        
+        if (variables.ContainsKey("working_directory"))
+        {
+            var optionWorkingDir = variables["working_directory"].AsString();
+            if (optionWorkingDir.IsSome(out var workingDir))
+            {
+                targetPath = Path.Join(workingDir, targetPath);
+            }
+        }
+
+        try
+        {
+            if (File.Exists(targetPath))
+            {
+                File.Delete(targetPath);
+            }
+            else if (Directory.Exists(targetPath))
+            {
+                Directory.Delete(targetPath);
+            }
+        }
+        catch (Exception e)
+        {
+            ConsoleLibrary.Log(e.Message, LogType.Error);
+        }
+    }
+}
---
+++ ATL.Script/Actions/ScriptActionMove.cs
@@ -0,0 +1,96 @@
+﻿using System.Xml.Linq;
+using ATL.Core.Extensions;
+using ATL.Core.Libraries;
+using ATL.Script.Variables;
+using RustyOptions;
+
+namespace ATL.Script.Actions;
+
+public class ScriptActionMove : IScriptAction
+{
+    public static string NodeName { get; } = "move";
+    
+    public void Process(XElement element, Dictionary<string, ScriptVariable> variables)
+    {
+        var fromAttr = element.Attribute("from");
+        if (fromAttr is null)
+            return;
+        
+        var toAttr = element.Attribute("to");
+        if (toAttr is null)
+            return;
+
+        var targetFrom = fromAttr.Value;
+        var targetTo = toAttr.Value;
+
+        if (targetFrom.StartsWith(ScriptVariable.NodeSymbol))
+        {
+            var fromVarName = targetFrom.Replace(ScriptVariable.NodeSymbol, "");
+            
+            var optionFromVar = variables.GetValueOrNone(fromVarName);
+            if (!optionFromVar.IsSome(out var targetFromVar))
+            {
+                return;
+            }
+
+            var optionFrom = targetFromVar.AsString();
+            if (!optionFrom.IsSome(out var fromVar))
+            {
+                return;
+            }
+            
+            targetFrom = fromVar;
+        }
+        
+        if (targetTo.StartsWith(ScriptVariable.NodeSymbol))
+        {
+            var toVarName = targetTo.Replace(ScriptVariable.NodeSymbol, "");
+            
+            var optionToVar = variables.GetValueOrNone(toVarName);
+            if (!optionToVar.IsSome(out var targetToVar))
+            {
+                return;
+            }
+
+            var optionTo = targetToVar.AsString();
+            if (!optionTo.IsSome(out var toVar))
+            {
+                return;
+            }
+            
+            targetTo = toVar;
+        }
+
+        if (variables.ContainsKey("working_directory"))
+        {
+            var optionWorkingDir = variables["working_directory"].AsString();
+            if (optionWorkingDir.IsSome(out var workingDir))
+            {
+                targetFrom = Path.Join(workingDir, targetFrom);
+                targetTo = Path.Join(workingDir, targetTo);
+            }
+        }
+
+        try
+        {
+            if (File.Exists(targetFrom))
+            {
+                if (File.Exists(targetTo))
+                    File.Delete(targetTo);
+                
+                File.Move(targetFrom, targetTo);
+            }
+            else if (Directory.Exists(targetFrom))
+            {
+                if (Directory.Exists(targetTo))
+                    Directory.Delete(targetTo);
+                
+                Directory.Move(targetFrom, targetTo);
+            }
+        }
+        catch (Exception e)
+        {
+            ConsoleLibrary.Log(e.Message, LogType.Error);
+        }
+    }
+}
